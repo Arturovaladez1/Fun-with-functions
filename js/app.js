dec9659a628063500cbba52651155945ed19cf86
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-01-27 17:32:10 -0800

    Test4branch

--- js/app.js
+++ js/app.js
@@ -87,12 +87,18 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+  let addition = sum(testArray[0], testArray[1])[0];
+  console.log(addition);
+  let lastinteger = sum(addition, testArray[2])[0];
+  console.log(lastinteger);
+  let message = `${testArray[0]},${testArray[1]},${testArray[2]} was passed in as an array of numbers, and ${lastinteger} is their sum.`;
+  return [lastinteger, message];
 
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
