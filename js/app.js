2773a84fab09418c0539dded8b300de2368239f9
Arturo Cunillas Valadez <Arturo.valadez1@aol.com>
2023-01-27 16:22:12 -0800

    Test3Branch

--- js/app.js
+++ js/app.js
@@ -54,11 +54,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let addition = sum(a,b)[0];
+  addition = sum(addition, c)[0];
+  console.log(addition);
 
+  let product = multiply(a, b)[0];
+  product = multiply(product, c)[0];
+  console.log(product);
+
+  let addString = `${a} and ${b} and ${c} sum to ${addition}.`;
+  let multString = `The product of ${a} and ${b} and ${c} is ${product}.`;
+
+  return [addition, product, addString, multString];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
